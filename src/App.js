d7ee92b355b8c4177b9b2f2192856a189e981eb9
Aaron Berns <anberns@gmail.com>
2019-03-22 19:09:44 -0400

    add homepage container, route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import StudentsPage from './Containers/StudentsPage';
+import HomePage from './Containers/HomePage';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <React.Fragment>
+          <Route exact path="/" component={HomePage} />
           <Route exact path="/students" component={StudentsPage} />
         </React.Fragment>
       </Router>)

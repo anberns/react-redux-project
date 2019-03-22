81e614cb0b692c79aa273ed4ec9805e9b78cdc73
Aaron Berns <anberns@gmail.com>
2019-03-22 18:57:20 -0400

    copy over deleteStudent action

---
+++ src/actions/deleteStudent.js
@@ -0,0 +1,17 @@
+export function deleteStudent(id) {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_DELETE_STUDENT' });
+    return fetch(`http://localhost:3000/students/${id}`, {
+    //return fetch('https://rapid-exchange-api.herokuapp.com/students/create', {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        id: id
+      })
+    })
+      .then(response => response.json())
+      .then(students => dispatch({ type: 'SHOW_STUDENTS', students}));
+  };
+}

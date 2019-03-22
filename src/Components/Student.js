bb287cc6a3fd51c43b74ac9d5db34e78e513f9ad
Aaron Berns <anberns@gmail.com>
2019-03-22 19:03:57 -0400

    add delete button and handler

--- src/Components/Student.js
+++ src/Components/Student.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 export default class Student extends Component {
+  
+  handleOnClickDelete() {
+    this.props.deleteStudent(this.props.id)
+  }
 
   render() {
     
@@ -8,6 +12,7 @@ export default class Student extends Component {
         <div >
           <h1>{this.props.name}</h1>
           <h2>{this.props.stage}</h2>
+          <button onClick={() => this.handleOnClickDelete()}>Delete</button>
         </div>
     )
   }

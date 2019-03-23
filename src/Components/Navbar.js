57b655f788e347983822be458df91c12c76c346c
Aaron Berns <anberns@gmail.com>
2019-03-22 20:46:37 -0400

    refactor Navbar into presentational

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
-export default class Navbar extends Component {
-  render() {
-    return(
-    <div>
-      <nav className="navbar navbar-expand-lg nav-styled " >
-        <NavLink className="nav-item nav-link nav-brand-styled"
-          to="/"
-          /* set exact so it knows to only set activeStyle when route is deeply equal to link */
-          exact
-          /* add styling to Navlink */
-          /* add prop for activeStyle */
-        >Rapid Exchange</NavLink>
-        <NavLink className="nav-item nav-link nav-link-styled"
-          to="/users/1"
-          exact
-        >Profile</NavLink>
-        <NavLink className="nav-item nav-link nav-link-styled"
-          to="/students"
-          exact
-        >Students</NavLink>
+const Navbar = () => (
+  <div>
+    <nav className="navbar navbar-expand-lg nav-styled " >
+      <NavLink className="nav-item nav-link nav-brand-styled"
+        to="/"
+        /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+        exact
+        /* add styling to Navlink */
+        /* add prop for activeStyle */
+      >Rapid Exchange</NavLink>
+      <NavLink className="nav-item nav-link nav-link-styled"
+        to="/users/1"
+        exact
+      >Profile</NavLink>
+      <NavLink className="nav-item nav-link nav-link-styled"
+        to="/students"
+        exact
+      >Students</NavLink>
     </nav>
   </div>
-    )
-  }
-}
+)
+export default Navbar;
--- src/Components/User.js
+++ src/Components/User.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import './Student.css'
+import React from 'react';
+import './User.css'
 
 const User = (props) => (
-  <div className="card student-card">
-    <h3 className="card-header student-header">{props.user.name}</h3>
+  <div className="card user-card">
+    <h3 className="card-header user-header">{props.user.name}</h3>
     <div className="card-body">
       <h6>{props.user.email}</h6>
     </div>

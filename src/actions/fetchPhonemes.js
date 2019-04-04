ab64d3249145123f8125b55d9f3f9446e9e5958e
Aaron Berns <anberns@gmail.com>
2019-04-04 09:05:33 -0400

    create fetchPhonemes action creator

---
+++ src/actions/fetchPhonemes.js
@@ -0,0 +1,9 @@
+export default function fetchPhonemes() {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_PHONEMES' });
+    return fetch('http://localhost:3000/phonemes')
+    //return fetch('https://rapid-exchange-api.herokuapp.com/students')
+      .then(response => response.json())
+      .then(phonemes=> dispatch({ type: 'FETCH_PHONEMES', phonemes}));
+  };
+}

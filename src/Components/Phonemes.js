9e6279fcd2301d580306eb49288a3823af78226c
Aaron Berns <anberns@gmail.com>
2019-04-04 09:26:30 -0400

    refactor list creation in Phonemes

--- src/Components/Phonemes.js
+++ src/Components/Phonemes.js
@@ -4,25 +4,31 @@ import Phoneme from './Phoneme';
 export default class Phonemes extends Component {
   render() {
     const phonemesArr = Object.values(this.props.phonemes)[0]
-    const begList = phonemesArr.filter(phoneme => phoneme.classification === "beg")
-    const midList = phonemesArr.filter(phoneme => phoneme.classification === "mid")
-    const endList = phonemesArr.filter(phoneme => phoneme.classification === "end")
-    const phonemeList = phonemesArr.map(phoneme => {
-      return (
-        <div>
-          <Phoneme
-            id={phoneme.id}
-            key={phoneme.id}
-            characters={phoneme.characters}
-            classification={phoneme.classification}
-          />
-        </div>
-      )
-    })
+    const begList = createPhonemeList(phonemesArr, "beg")
+    const midList = createPhonemeList(phonemesArr, "mid")
+    const endList = createPhonemeList(phonemesArr, "end")
     return (
-    <ul>
-      {phonemeList}
-    </ul>
+      <ul>
+        {begList}
+        {midList}
+        {endList}
+      </ul>
     )
   }
+}
+
+const createPhonemeList= ( arr, classification ) => {
+  const list = arr.filter(phoneme => phoneme.classification === classification).map(phoneme => {
+    return (
+      <div>
+        <Phoneme
+          id={phoneme.id}
+          key={phoneme.id}
+          characters={phoneme.characters}
+          classification={phoneme.classification}
+        />
+      </div>
+    )
+  })
+  return list;
 }

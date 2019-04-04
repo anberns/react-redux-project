c363fea0f271a049240fc107193a74722a0120ae
Aaron Berns <anberns@gmail.com>
2019-04-04 09:08:20 -0400

    add phonemes reducer to root reducers

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 import studentsReducer from './studentsReducer';
+import phonemesReducer from './phonemesReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
-  students: studentsReducer
+  students: studentsReducer,
+  phonemes: phonemesReducer
 })
 
 export default rootReducer;

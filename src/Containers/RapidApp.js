bb99091213d08c16c15bb68163c5023abe03510e
Aaron Berns <anberns@gmail.com>
2019-04-10 09:27:11 -0400

    add initial state to RapidApp component

--- src/Containers/RapidApp.js
+++ src/Containers/RapidApp.js
@@ -1,6 +1,27 @@
+import React, { Component, useState } from 'react'
+
 class RapidApp extends Component {
-  
+
+  state = {
+    phonemes: [
+      { content: "", vowel: false },
+      { content: "", vowel: true },
+      { content: "", vowel: false }
+    ],
+    phonemeLists: [
+      { list: [] },
+      { list: [] },
+      { list: [] }
+    ],
+    soundPositions: [
+      { position: 0 },
+      { position: 0 },
+      { position: 0 }
+    ],
+    eToggle: false
+  }
   render() {
+  
     return (
       />
     )

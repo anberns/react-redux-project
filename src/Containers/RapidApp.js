22c9ef977f8cf88b3660852d1cc36060d67db0f0
Aaron Berns <anberns@gmail.com>
2019-04-10 09:32:56 -0400

    load phonemeLists with redux store chosen lists

--- src/Containers/RapidApp.js
+++ src/Containers/RapidApp.js
@@ -1,25 +1,29 @@
-import React, { Component, useState } from 'react'
+import React, { Component } from 'react'
 
 class RapidApp extends Component {
 
   state = {
+    // phoneme cards that display beg, mid, end phonemes respectively
     phonemes: [
       { content: "", vowel: false },
       { content: "", vowel: true },
       { content: "", vowel: false }
     ],
+    // chosen phonemes for beg, mid, end respectively
     phonemeLists: [
-      { list: [] },
-      { list: [] },
-      { list: [] }
+        { list: [...this.state.chosenBeg] },
+        { list: [...this.state.chosenMid]  },
+        { list: [...this.state.chosenEnd] }
     ],
+    // current index of displayed phoneme from phonemeLists
     soundPositions: [
-      { position: 0 },
-      { position: 0 },
-      { position: 0 }
+        { position: 0 },
+        { position: 0 },
+        { position: 0 }
     ],
-    eToggle: false
+    eToggle: false // v-e card displayed
   }
+
   render() {
   
     return (

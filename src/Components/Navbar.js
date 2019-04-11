90b1f9a8dbc308612f4647d4884b19bf4fb78a7f
Aaron Berns <anberns@gmail.com>
2019-04-11 10:44:23 -0400

    import bootstrap to Navbar to enable collapse

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import NavItem from './NavItem';
 import './Navbar.css';
+import '../../node_modules/bootstrap/dist/js/bootstrap.min.js'
 
 const Navbar = () => (
   <div>
-    <nav className="navbar navbar-expand-lg nav-styled " >
-      <NavItem path="/" linkName="Rapid Exchange" />
-      <NavItem path="/users/1" linkName="Profile" />
-      <NavItem path="/users/1/students" linkName="Students" />
+    <nav className="navbar navbar-expand-lg navbar-light nav-styled " >
+      <NavItem path="/" linkName="Rapid Exchange" className="navbar-brand"/>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <NavItem path="/users/1" linkName="Profile" className="nav-item nav-link"/>
+          <NavItem path="/users/1/students" linkName="Students" className="nav-item nav-link" />
+        </div>
+      </div>
     </nav>
   </div>
 )

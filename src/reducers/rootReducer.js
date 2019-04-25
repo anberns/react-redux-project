25703860d534ddd19e5dc5c56ee03cee9802825e
Aaron Berns <anberns@gmail.com>
2019-04-25 13:45:51 -0400

    create empty authReducer to be used for signup, signin

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 import studentsReducer from './studentsReducer';
 import phonemesReducer from './phonemesReducer';
+import authReducer from './authReducer';
 
 const rootReducer = combineReducers({
+  auth: authReducer,
   user: userReducer,
   students: studentsReducer,
   phonemes: phonemesReducer

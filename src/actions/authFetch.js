9f8f3d475abc43632d2724c68fa834c5229f46a4
Aaron Berns <anberns@gmail.com>
2019-04-25 13:33:45 -0400

    add fetch auth/login to authFetch

--- src/actions/authFetch.js
+++ src/actions/authFetch.js
@@ -1,8 +1,17 @@
 export default function authFetch(email, password) {
   return (dispatch) => {
     dispatch({ type: 'AUTHENTICATING' });
-    return fetch()//`http://localhost:3000/users/${id}`)
-    //return fetch('https://rapid-exchange-api.herokuapp.com/students')
+    return fetch('http://localhost:3000/auth/login', {
+      //return fetch('https://rapid-exchange-api.herokuapp.com/students/create', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
+      })
       .then(response => response.json())
       .then(user => dispatch({ type: 'AUTH_FETCH', user}));
   };

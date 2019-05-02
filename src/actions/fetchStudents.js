9f26a3e67f19dd1ea74835fc6408a1a5734c4f8e
Aaron Berns <anberns@gmail.com>
2019-05-02 08:22:49 -0400

    change fetch url to heroku db in all fetch actions

--- src/actions/fetchStudents.js
+++ src/actions/fetchStudents.js
@@ -1,8 +1,8 @@
 export default function fetchStudents() {
   return (dispatch) => {
     dispatch({ type: 'LOADING_STUDENTS' });
-    return fetch('http://localhost:3000/students')
-    //return fetch('https://rapid-exchange-api.herokuapp.com/students')
+    //return fetch('http://localhost:3000/students')
+    return fetch('https://rapid-exchange-api.herokuapp.com/students')
       .then(response => response.json())
       .then(students => dispatch({ type: 'FETCH_STUDENTS', students}));
   };
--- src/actions/fetchUser.js
+++ src/actions/fetchUser.js
@@ -1,8 +1,8 @@
 export default function fetchUser(id) {
   return (dispatch) => {
     dispatch({ type: 'LOADING_USER' });
-    return fetch(`http://localhost:3000/users/${id}`)
-    //return fetch('https://rapid-exchange-api.herokuapp.com/students')
+    //return fetch(`http://localhost:3000/users/${id}`)
+    return fetch(`https://rapid-exchange-api.herokuapp.com/users/${id}`)
       .then(response => response.json())
       .then(user => dispatch({ type: 'FETCH_USER', user}));
   };

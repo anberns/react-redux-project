10b7dcc3e5a5a654b4c5284c32e28f3e839ed6c1
Aaron Berns <anberns@gmail.com>
2019-05-02 08:59:18 -0400

    more landing page styling

--- src/Containers/HomePage.js
+++ src/Containers/HomePage.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ChoiceSelector from './ChoiceSelector'
 
 const HomePage = () => (
-<div className="selectorDivCenter tempLanding">
-  <h1>Welcome to the Rapid Exchange Application</h1>  
-  <div className="selectorDivJustify">
+<div className="selectorDivCenter" id="tempLanding">
+  <h1>Welcome to the Rapid Exchange Application</h1><br></br> 
+  <div className="selectorDivJustify" id="tempNotes">
     <p>This is a work in progress. The actual application is functional and can be used by clicking on 'App'.</p>
-    <p>The 'Profile' and 'Students' links lead to dynamically rendered content that is, for the moment, hard coded into the database.</p>
+    <p>The 'Profile' and 'Students' links lead to content that is, for the moment, hard coded into the database.</p>
     <p>I'm currently working on implementing authentication / signup / login features to allow users to store student information that will personalize the application.</p>
     <p>***This application is connected to an API that is hosted on Heroku at its free tier, so the first time you try it, the dynamic content may take a while as the dyno spins up.***</p>
     </div>
